5d96087cd2eab93ebd7d21368636ce275c30cbda
Cristina <kristikavai@mail.ru>
2024-10-29 21:05:16 +0300

    add main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 import os
 
-def clear_names(file_names: str)-> list:
+
+def clear_names(file_names: str) -> list:
     """Функция для очистки имен от лишних символов"""
     new_names_list = list()
     file_names_adress = os.path.abspath(file_names)
-    with open(file_names_adress + , "r") as names_file:
+    with open(file_names_adress, "r", encoding="utf-8") as names_file:
         names_list = names_file.read().split()
         for name_item in names_list:
-            new_name=""
+            new_name = ""
             for symbol in name_item:
                 if symbol.isalpha():
                     new_name += symbol
@@ -16,8 +17,8 @@ def clear_names(file_names: str)-> list:
     return new_names_list
 
 
-if __name__== "__main__":
-    cleared_names = clear_names("names.txt")
 
-    for i in cleared_names:
-        print(i)
+cleared_names = clear_names("names.txt")
+
+for i in cleared_names:
+    print(i)
